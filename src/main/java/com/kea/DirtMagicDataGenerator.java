00c79bdd33eedabb8ddd96e74858f30be552eda2
Keadehara233 <1206607402@qq.com>
2025-07-28 15:57:54 +0800

    a magic mod!

---
+++ src/main/java/com/kea/DirtMagicDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kea;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DirtMagicDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/kea/DirtMagicModClient.java
@@ -0,0 +1,10 @@
+package com.kea;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DirtMagicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
