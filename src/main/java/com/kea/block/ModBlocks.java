958c9591e2950c22bb2b97664635a9fe29cba5e1
Keadehara233 <1206607402@qq.com>
2025-07-29 14:46:56 +0800

    the first block!

--- src/main/java/com/kea/block/ModBlocks.java
+++ src/main/java/com/kea/block/ModBlocks.java
@@ -1,4 +1,11 @@
 package com.kea.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
 public class ModBlocks {
+    public static Block register(String id, Block block) {
+        return Registry.register(Registries.BLOCK, id, block);
+    }
 }
